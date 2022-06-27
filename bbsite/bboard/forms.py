a61f96ca06b034600f1efbf1ac64462e78a010d5
DmitriyDedunik <Dedunik.Dmitry@yandex.ru>
2022-06-27 17:39:03 +0300

    добавили описание ошибки и описание полей

--- bbsite/bboard/forms.py
+++ bbsite/bboard/forms.py
@@ -1,14 +1,22 @@
-from dataclasses import fields
-from msilib.schema import Class
-from pyexpat import model
-from unicodedata import name
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, Textarea
 from .models import Bb, Rubric
 
 class BbForm(ModelForm):
     class Meta:
         model = Bb
         fields = ('title', 'price', 'content', 'rubric')
+
+        widgets = {
+            'title': TextInput(attrs={
+                'PlaceHolder': 'Наименование'
+            }),
+            'content': Textarea(attrs={
+                'PlaceHolder': 'Описание'
+            }),
+            'rubric': TextInput(attrs={
+                'PlaceHolder': 'Рубрика'
+            }),              
+            }
 
 class RubricForm(ModelForm):
     class Meta:
