ef12857c684458adff5ac0856c4dc0bf5a896bde
DmitriyDedunik <Dedunik.Dmitry@yandex.ru>
2022-07-01 01:12:40 +0300

    перенос бд на postgre

--- bbsite/bbsite/settings.py
+++ bbsite/bbsite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bbsite',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,10 +76,21 @@ WSGI_APPLICATION = 'bbsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bbsite', 
+        'USER': 'postgres', 
+        'PASSWORD': '987654Qq',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
